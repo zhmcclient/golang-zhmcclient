bcad3181c7601d9eb804a31738d0beeebfa808fc
Rahul Bhalla <bhallar@in.ibm.com>
2023-04-20 18:43:26 +0530

    Add Go Vuln Check (#74)
    * Add Go Vuln Check
    
    * Updated travis build vul check
    
    * Updated test script with flags
    
    * Add git private repo script
    
    * Added test script flag
    
    * Removed script variables from travis file
    
    * Updated go versions
    
    * Disabled caching
    
    * Removed GOPRIVATE env var
    
    * Reverted travis changes
    
    * Added govulcheck json output
    
    * Updated go version in travis config
    
    * Added package for go-test
    
    * Update go packages and version to 1.20

--- go-test.sh
+++ go-test.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 args=("$@")
-package=${args[0]}
-file=${args[1]}
+v_flag=''
+f_flag=''
+p_flag=''
+
+while getopts 'fpv:' flag; do
+  case "${flag}" in
+    f) 
+        file=${OPTARG}
+        f_flag='true'
+        ;;
+    v) 
+        package=${OPTARG}
+        v_flag='true'
+        ;;
+    p) 
+        package=${OPTARG}
+        p_flag='true'
+        ;;
+    *) exit 1
+       ;;
+  esac
+done
 
 coveroutput="cover.out"
 tmpfile="tmp.out"
@@ -30,53 +50,73 @@ function set_env() {
   git config --global credential.helper "store --file=${GIT_CRED_FILE}"
 }
 
+function run_vul_check() {
+  package=${args[1]}
+  go install golang.org/x/vuln/cmd/govulncheck@latest
+  scan_result=`govulncheck -json ./... | jq -r '.vulnerability | {cves: .osv.aliases, id: .osv.id, details: .osv.details, packages: .modules, url: .osv.database_specific.url}' | jq --sort-keys | jq -s .| jq '.[] | with_entries( select( .value != null and . !={} )) | del(..|select(. == {}))| select( . != null)' | jq -s .`
+  echo "$scan_result"
+  vul_count=`echo $scan_result | jq length`
+  echo "$vul_count found in go package: $package"
+}
+
+function run_unit_tests() {
+
+  file=${args[2]}
+  package=${args[1]}
+
+  test_coversummary=`go test -v -cover ./... | grep  -E -A15 --text "(^\-\-\-\-|Fail|Pass).*"`
+  test_cover_result=`echo "$test_coversummary" | grep --text "^--- "| cut -d " " -f2| sed "s/://g"`
+
+  #check if the test cases are passing before getting the coverage
+  if [[ $test_cover_result == "FAIL" ]];then
+    echo -e "$test_coversummary"
+    exit 1
+  fi
+  go_test_coverout=`go test ./$package -coverprofile=$coveroutput`
+  go tool cover -func=$coveroutput > $tmpfile
+  unlink $coveroutput
+  coveragesum=0
+  filecounter=0
+  if [[ $file != "" ]]; then
+    while read line; do
+      regex=".*"$file+"\.go.*"
+      if [[ $line =~ $regex ]]; then
+        echo $line
+        coverage=`echo "$line" | awk '{print $3}' | sed -E "s/\..*%//g"`
+        filecounter=$((filecounter + 1))
+        coveragesum=$((coveragesum + coverage))
+      fi
+    done < $tmpfile;
+    unlink $tmpfile
+    if [[ $coveragesum != 0  ]];then
+      echo ""
+      echo "Coverage for $file.go: $(( $coveragesum / $filecounter))%"
+    else
+      echo "No coverage found for the $file.go"
+    fi
+  else
+    go_test_coverout=`go test ./$package -coverprofile=$coveroutput`
+    go tool cover -func=$coveroutput
+    unlink $coveroutput
+  fi
+
+  test_result=`echo $go_test_coverout | cut -d ":" -f2 | cut -d " " -f2 | awk '{printf("%d\n",$0+=$0<0?0:0.9)}'`
+  # If coverage is below 65 then fail the build
+  if [[ $test_result -lt 65 ]]; then
+    exit 1
+  fi
+}
+
 check_env
 
-
-if [[ $package =~ ^[a-z]{0,2}$ ]];
-  then
-    echo "Invalid package name, please enter valid package and retry command"
-    exit 1
+if [[  $f_flag == 'true'  ]]; then
+  run_unit_tests $package $file
 fi
 
-test_coversummary=`go test -v -cover ./... | grep  -E -A15 --text "(^\-\-\-\-|Fail|Pass).*"`
-test_cover_result=`echo "$test_coversummary" | grep --text "^--- "| cut -d " " -f2| sed "s/://g"`
-
-#check if the test cases are passing before getting the coverage
-if [[ $test_cover_result == "FAIL" ]];then
-  echo -e "$test_coversummary"
-  exit 1
-fi
-go_test_coverout=`go test ./$package -coverprofile=$coveroutput`
-go tool cover -func=$coveroutput > $tmpfile
-unlink $coveroutput
-coveragesum=0
-filecounter=0
-if [[ $file != "" ]]; then
-  while read line; do
-    regex=".*"$file+"\.go.*"
-    if [[ $line =~ $regex ]]; then
-      echo $line
-      coverage=`echo "$line" | awk '{print $3}' | sed -E "s/\..*%//g"`
-      filecounter=$((filecounter + 1))
-      coveragesum=$((coveragesum + coverage))
-    fi
-  done < $tmpfile;
-  unlink $tmpfile
-  if [[ $coveragesum != 0  ]];then
-    echo ""
-    echo "Coverage for $file.go: $(( $coveragesum / $filecounter))%"
-  else
-    echo "No coverage found for the $file.go"
-  fi
-else
-  go_test_coverout=`go test ./$package -coverprofile=$coveroutput`
-  go tool cover -func=$coveroutput
-  unlink $coveroutput
+if [[  $v_flag == 'true'  ]]; then
+  run_vul_check $package
 fi
 
-test_result=`echo $go_test_coverout | cut -d ":" -f2 | cut -d " " -f2 | awk '{printf("%d\n",$0+=$0<0?0:0.9)}'`
-# If coverage is below 65 then fail the build
-if [[ $test_result -lt 65 ]]; then
-  exit 1
+if [[  $p_flag == 'true'  ]]; then
+  run_unit_tests $package
 fi
--- go.mod
+++ go.mod
@@ -1,9 +1,37 @@
 module github.ibm.com/zhmcclient/golang-zhmcclient
 
-go 1.16
+go 1.20
 
 require (
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.18.1
-	github.ibm.com/genctl/shared-logger v1.4.0 // indirect
+	github.ibm.com/genctl/shared-logger v1.4.0
+)
+
+require (
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gogo/protobuf v1.2.2-0.20190723190241-65acae22fc9d // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gofuzz v1.0.0 // indirect
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/uber/jaeger-client-go v2.29.1+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.18.1 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.27.1 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.17.17 // indirect
+	k8s.io/klog v1.0.0 // indirect
 )
